d1017732ff7bc4b17050d46da7c81644dc6fd965
Taranchuk <nfhfyxer@gmail.com>
2024-01-25 18:46:48 +0200

    pregnancy precepts, pronouns fix

---
+++ 1.4/Source/Precepts/ThoughtWorker_Precept_NotPregnant.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ReSpliceCharmweavers
+{
+    public class ThoughtWorker_Precept_NotPregnant : ThoughtWorker_Precept
+    {
+        public override ThoughtState ShouldHaveThought(Pawn p)
+        {
+            return p.gender == Gender.Female && PregnancyUtility.GetPregnancyHediff(p) is null;
+        }
+    }
+}
