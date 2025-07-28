d34534569f5e5c8a569ff69f53cfc07382f0bbe0
SokyranTheDragon <exploder1531@gmail.com>
2025-07-28 22:46:09 +0200

    Small VRE-H compat fix
    Using initiate lovin' ability from VRE-H won't cause breakups between a master and their thrall.

---
+++ 1.6/Source/HarmonyPatches/VRE_Highmate_Breakup_Patch.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace ReSpliceCharmweavers;
+
+[HarmonyPatch("VanillaRacesExpandedHighmate.JobDriver_InitiateLovin", "ProcessBreakups")]
+public static class VRE_Highmate_Breakup_Patch
+{
+    public static bool Prepare(MethodBase method) => method != null || ModsConfig.IsActive("vanillaracesexpanded.highmate");
+
+    // Don't force breakups between a master and their thrall
+    public static bool Prefix(Pawn initiator, Pawn recipient)
+        => !initiator.relations.DirectRelationExists(RS_DefOf.RS_Master, recipient) && !initiator.relations.DirectRelationExists(RS_DefOf.RS_Thrall, recipient);
+}
