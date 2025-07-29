e6a429bedb00e933098b6184cdf0623e20343150
SokyranTheDragon <exploder1531@gmail.com>
2025-07-29 16:52:21 +0200

    Update compat

---
+++ 1.6/Source/HarmonyPatches/VRE_Highmate_RJW_InitiateLoving_Patch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace ReSpliceCharmweavers;
+
+[HarmonyPatch("VanillaRacesExpandedHighmate.CompAbilityEffect_InitiateLovin", "Apply")]
+[HarmonyPatch([typeof(LocalTargetInfo), typeof(LocalTargetInfo)])]
+public static class VRE_Highmate_RJW_InitiateLoving_Patch
+{
+    public static bool Prepare(MethodBase method) => method != null || ModsConfig.IsActive("vanillaracesexpanded.highmate");
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        // Replace the VRE job with the one from RJW, as the VRE one does not interact with RJW systems.
+        var targetField = AccessTools.DeclaredField("VanillaRacesExpandedHighmate.InternalDefOf:VRE_InitiateLovin");
+        var replacementField = AccessTools.DeclaredField("rjw.xxx:quick_sex");
+
+        foreach (var instr in instructions)
+        {
+            if (instr.LoadsField(targetField))
+                instr.operand = replacementField;
+
+            yield return instr;
+        }
+    }
+}
