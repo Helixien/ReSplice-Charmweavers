3cebfbb10a37b91d44c94fa7744f974edddb7959
SokyranTheDragon <exploder1531@gmail.com>
2025-08-03 22:34:15 +0200

    Multi-pregnancy gene allows for more frequent ovum extraction

---
+++ 1.6/Source/HarmonyPatches/Recipe_ExtractOvum_OnSurgerySuccess_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ReSpliceCharmweavers;
+
+[HarmonyPatch(typeof(Recipe_ExtractOvum), nameof(Recipe_ExtractOvum.OnSurgerySuccess))]
+public static class Recipe_ExtractOvum_OnSurgerySuccess_Patch
+{
+    private static void Postfix(Pawn pawn)
+    {
+        if (pawn.genes?.HasActiveGene(RS_DefOf.RS_MultiPregnancy) == true)
+        {
+            var disappearsComp = pawn.health?.hediffSet?.GetFirstHediffOfDef(HediffDefOf.OvumExtracted)?.TryGetComp<HediffComp_Disappears>();
+            if (disappearsComp != null)
+                disappearsComp.ticksToDisappear = GenDate.TicksPerDay;
+        }
+    }
+}
