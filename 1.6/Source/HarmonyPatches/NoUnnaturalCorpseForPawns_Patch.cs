18029f9335be6b24de405703f8c43ad3ca5feeed
SokyranTheDragon <exploder1531@gmail.com>
2025-08-05 16:18:46 +0200

    Thralls can't have unnatural corpses

---
+++ 1.6/Source/HarmonyPatches/NoUnnaturalCorpseForPawns_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.QuestGen;
+using Verse;
+
+namespace ReSpliceCharmweavers;
+
+[HarmonyPatch]
+public static class NoUnnaturalCorpseForPawns_Patch
+{
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        // We could technically just patch GameComponent_Anomaly:PawnHasUnnaturalCorpse.
+        // However, this feels like a quick path towards causing bugs due to mod incompatibilities.
+        yield return typeof(IncidentWorker_UnnaturalCorpseArrival).DeclaredMethod("ValidatePawn");
+        yield return typeof(QuestNode_Root_MysteriousCargoUnnaturalCorpse).DeclaredMethod("ValidatePawn");
+    }
+
+    private static void Postfix(Pawn pawn, ref bool __result)
+    {
+        if (__result && pawn.IsLovethrall())
+            __result = false;
+    }
+}
