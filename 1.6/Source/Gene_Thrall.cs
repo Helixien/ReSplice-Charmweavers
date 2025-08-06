4b2ea3114f3623808d38537e6d4d6ed96d33e675
SokyranTheDragon <exploder1531@gmail.com>
2025-08-07 01:54:40 +0200

    Trait changes chance on losing gene

--- 1.6/Source/Gene_Thrall.cs
+++ 1.6/Source/Gene_Thrall.cs
@@ -1,9 +1,44 @@
-﻿using Verse;
+﻿using RimWorld;
+using Verse;
 
 namespace ReSpliceCharmweavers;
 
 public class Gene_Thrall : Gene
 {
+    public override void PostRemove()
+    {
+        base.PostRemove();
+
+        // 10% chance to keep changes to sexuality after no longer lovethralled
+        if (pawn.story?.traits != null && Rand.Chance(0.1f))
+        {
+            // If the gene was suppressing any of the sexuality traits, remove those (unless added by traits)
+            if (def.suppressedTraits != null)
+            {
+                foreach (var trait in def.suppressedTraits)
+                {
+                    if (trait.def == TraitDefOf.Bisexual || trait.def == TraitDefOf.Gay || trait.def == TraitDefOf.Asexual)
+                    {
+                        var current = pawn.story.traits.GetTrait(trait.def);
+                        // Only remove if not forced by other genes
+                        if (current is { sourceGene: null })
+                            pawn.story.traits.RemoveTrait(current);
+                    }
+                }
+            }
+
+            // If the gene was forcing any of the sexuality traits, add those (unless there's already any)
+            if (def.forcedTraits != null && !pawn.story.traits.HasTrait(TraitDefOf.Bisexual) && !pawn.story.traits.HasTrait(TraitDefOf.Gay) && !pawn.story.traits.HasTrait(TraitDefOf.Asexual))
+            {
+                foreach (var trait in def.forcedTraits)
+                {
+                    if (trait.def == TraitDefOf.Bisexual || trait.def == TraitDefOf.Gay || trait.def == TraitDefOf.Asexual)
+                        pawn.story.traits.GainTrait(new Trait(trait.def, trait.degree));
+                }
+            }
+        }
+    }
+
     public override void ExposeData()
     {
         base.ExposeData();
